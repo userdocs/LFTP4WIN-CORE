aa619e7cfffe37a182251bb58444326a6564a78f
userdocs <16525024+userdocs@users.noreply.github.com>
2019-12-09 23:03:13 +0000

    iperf3 - allow user to choose a port when running the custom command and/or set a persistent port in the config

--- system/applications/winscp/winscp-iperf3.WinSCPextension.sh
+++ system/applications/winscp/winscp-iperf3.WinSCPextension.sh
@@ -1,5 +1,5 @@
 # @name iperf3
-# @command cmd /c start "" "%WINSCP_PATH%\..\kitty\kitty_portable.exe" -pw "!P" "!U@!@" -P "!#" -title "!N" -classname "iperf3" -cmd "bash <(curl -4sL https://git.io/fjRIi)" && start "" "%WINSCP_PATH%\..\conemu\ConEmu64.exe" -run {Bash::bash} "%EXTENSION_PATH%" "!U" "!@" "!#" "!S" "!/" "!\" "!P"
+# @command cmd /c start "" "%WINSCP_PATH%\..\kitty\kitty_portable.exe" -pw "!P" "!U@!@" -P "!#" -title "!N" -classname "iperf3" -cmd "export IPERF3PORT=%IPERF3PORT% && bash <(curl -4sL https://git.io/fjRIi)" && start "" "%WINSCP_PATH%\..\conemu\ConEmu64.exe" -run {Bash::bash} "%EXTENSION_PATH%" "!U" "!@" "!#" "!S" "!/" "!\" "!P"
 # @side Local
 # @flag
 # @description Install iperf3 on the remote server and run a test using iperf3 and mtr. A random remote port is used so you may need to disable your firewall temporarily.
@@ -7,6 +7,7 @@
 # @version 1.0
 # @homepage https://github.com/userdocs/LFTP4WIN-CORE
 #
+# @option IPERF3PORT -config -run textbox "Select a port to override the random generation" ""
 #! /usr/bin/env bash
 #
 winscp_to_bash "$1" "$2" "$3" "$4" "$5" "$6" "$7"
@@ -20,21 +21,27 @@ pget -c '~/.iperf3port' -o "/tmp"
 quit
 EOF
 #
-echo 'Generating iperf3 report...'
-echo
-iperf3 -p "$(cat "/tmp/.iperf3port")" -c "$hostname" -R | tee "$HOME/../help/reports/report-$hostname.txt"
+echo 'Generating iperf3 report - Server to Client' | tee "$HOME/../help/reports/report-$hostname.txt"
+echo | tee -a "$HOME/../help/reports/report-$hostname.txt"
 #
-echo
+iperf3 -4 -p "$(cat "/tmp/.iperf3port")" -c "$hostname" -R | tee -a "$HOME/../help/reports/report-$hostname.txt"
+#
+echo | tee -a "$HOME/../help/reports/report-$hostname.txt"
+echo 'Generating iperf3 report - Client to Server' | tee -a "$HOME/../help/reports/report-$hostname.txt"
+echo | tee -a "$HOME/../help/reports/report-$hostname.txt"
+#
+iperf3 -4 -p "$(cat "/tmp/.iperf3port")" -c "$hostname" | tee -a "$HOME/../help/reports/report-$hostname.txt"
 #
 rm -f /tmp/.iperf3port
 #
-echo 'Generating MTR report...'
-echo
+echo | tee -a "$HOME/../help/reports/report-$hostname.txt"
+echo 'Generating MTR report...' | tee -a "$HOME/../help/reports/report-$hostname.txt"
+echo | tee -a "$HOME/../help/reports/report-$hostname.txt"
 mtr -r4 "$hostname" | tee -a "$HOME/../help/reports/report-$hostname.txt"
 #
-echo
-echo 'This report has been saved to the help/reports directory.'
-echo
+echo | tee -a "$HOME/../help/reports/report-$hostname.txt"
+echo 'This report has been saved to the help/reports directory.' | tee -a "$HOME/../help/reports/report-$hostname.txt"
+echo | tee -a "$HOME/../help/reports/report-$hostname.txt"
 read -ep "Are you ready to close the terminals?: " -i "y" quitme
 #
 if [[ "$quitme" =~ ^[Yy]$ ]]; then
--- system/scripts/install.iperf.sh
+++ system/scripts/install.iperf.sh
@@ -8,6 +8,7 @@ if [[ $(echo "$PATH" | grep -oc "$HOME/bin") -eq "0" && $(cat ~/.bashrc | grep -
 	export PATH="$HOME/bin:$PATH${PATH:+:${PATH}}"
 	echo 'export PATH="$HOME/bin${PATH:+:${PATH}}"' >> ~/.bashrc
 fi
+#
 if [[ $(echo "$LD_LIBRARY_PATH" | grep -oc "$HOME/lib") -eq "0" && $(cat ~/.bashrc | grep -oc 'export LD_LIBRARY_PATH="$HOME/lib${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}"') -eq "0" ]]; then
 	export LD_LIBRARY_PATH="$HOME/lib${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}"
 	echo 'export LD_LIBRARY_PATH="$HOME/lib${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}"' >> ~/.bashrc
@@ -16,17 +17,21 @@ fi
 wget -q4O ~/bin/iperf3 https://iperf.fr/download/ubuntu/iperf3_3.1.3 && chmod +x ~/bin/iperf3
 wget -q4O ~/lib/libiperf.so.0 https://iperf.fr/download/ubuntu/libiperf.so.0_3.1.3
 #
-while [[ "$(ss -ln | grep -co ''"$port"'')" -ge "1" ]]; do port="$(shuf -i 10001-32001 -n 1)"; done
-echo -n "$port" > ~/.iperf3port
+if [[ -z "$IPERF3PORT" ]]; then
+    IPERF3PORT="$(shuf -i 10001-32001 -n 1)" && while [[ "$(ss -ln | grep -co ''"$IPERF3PORT"'')" -ge "1" ]]; do IPERF3PORT="$(shuf -i 10001-32001 -n 1)"; done
+    echo -n "$IPERF3PORT" > ~/.iperf3port
+else
+    echo -n "$IPERF3PORT" > ~/.iperf3port
+fi
 #
 echo
 echo 'Connect with your client using these commands:'
 echo
-echo 'iperf3 -p '"$port"' -c '"$(curl -4s icanhazip.com)"''
-echo 'iperf3 -p '"$port"' -c '"$(curl -4s icanhazip.com)"' -R'
-echo 'iperf3 -p '"$port"' -c '"$(curl -4s icanhazip.com)"' -R -P 10'
-echo 'iperf3 -p '"$port"' -c '"$(curl -4s icanhazip.com)"' -R -u'
-echo 'iperf3 -p '"$port"' -c '"$(curl -4s icanhazip.com)"' -R -P 10 -u'
+echo 'iperf3 -p '"$IPERF3PORT"' -c '"$(curl -4s icanhazip.com)"''
+echo 'iperf3 -p '"$IPERF3PORT"' -c '"$(curl -4s icanhazip.com)"' -R'
+echo 'iperf3 -p '"$IPERF3PORT"' -c '"$(curl -4s icanhazip.com)"' -R -P 10'
+echo 'iperf3 -p '"$IPERF3PORT"' -c '"$(curl -4s icanhazip.com)"' -R -u'
+echo 'iperf3 -p '"$IPERF3PORT"' -c '"$(curl -4s icanhazip.com)"' -R -P 10 -u'
 echo
 #
-iperf3 -p $port -s
+iperf3 -p $IPERF3PORT -s
